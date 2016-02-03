a0c24304a8847c00c9ce12552b340c2dc95dddcc
Timothy Pulliam <timothy.dpulliam@gmail.com>
2016-02-03 03:57:29 +0000

    UnicodeEncodeError

--- crawler.py
+++ crawler.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from pprint import pprint
 import requests
 import MySQLdb
 from bs4 import BeautifulSoup
@@ -8,9 +7,9 @@ from bs4 import BeautifulSoup
 # Setup the database connection.
 connection = MySQLdb.connect(user="root", passwd="root", host="localhost")
 curs = connection.cursor()
-curs.execute("CREATE DATABASE IF NOT EXISTS crawler;")
+curs.execute("CREATE DATABASE IF NOT EXISTS crawler CHARACTER SET utf8;")
 curs.execute("CREATE TABLE IF NOT EXISTS crawler.urls (\
-url_id int NOT NULL,\
+url_id int NOT NULL AUTO_INCREMENT,\
 description VARCHAR(100),\
 url varchar(200),\
 PRIMARY KEY (url_id));")
@@ -18,7 +17,7 @@ PRIMARY KEY (url_id));")
 # start pulling html
 url = "http://www.reddit.com"
 r = requests.get(url)
-html = r.content
+html = r.content.decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 links = soup.find_all('a')
 
@@ -27,7 +26,7 @@ for link in links:
     data[link.text] = link.get("href")
 
 for key in data:
-    curs.execute("INSERT INTO TABLE crawler.urls VALUES (%s, %s);", (key, data[key])) 
+    curs.execute("INSERT INTO crawler.urls (description, url) VALUES (%s, %s);", (key, data[key])) 
 
 curs.close()
 connection.close()
