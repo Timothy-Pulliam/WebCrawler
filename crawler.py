3b15b97933986af525e6ccc858bffeaaffcea7ea
Timothy Pulliam <timothy.dpulliam@gmail.com>
2016-02-06 06:02:44 +0000

    Working crawler needs to be extensible

--- crawler.py
+++ crawler.py
@@ -5,19 +5,23 @@ import MySQLdb
 from bs4 import BeautifulSoup
 
 # Setup the database connection.
+url = "http://www.google.com"
+table_name = "google"
+create_database_sql = "CREATE DATABASE IF NOT EXISTS crawler CHARACTER SET utf8;"
+create_table_sql = "CREATE TABLE IF NOT EXISTS crawler.%s (\
+                    url_id int NOT NULL AUTO_INCREMENT,\
+                    description VARCHAR(1000),\
+                    url varchar(200),\
+                    PRIMARY KEY (url_id));" %(table_name)
+
 connection = MySQLdb.connect(user="root", passwd="root", host="localhost")
-curs = connection.cursor()
-curs.execute("CREATE DATABASE IF NOT EXISTS crawler CHARACTER SET utf8;")
-curs.execute("CREATE TABLE IF NOT EXISTS crawler.urls (\
-url_id int NOT NULL AUTO_INCREMENT,\
-description VARCHAR(100),\
-url varchar(200),\
-PRIMARY KEY (url_id));")
+cursor = connection.cursor()
+cursor.execute(create_database_sql)
+cursor.execute(create_table_sql)
 
 # start pulling html
-url = "http://www.reddit.com"
 r = requests.get(url)
-html = r.content.decode("utf-8")
+html = r.content.decode("utf-8", errors="replace")
 soup = BeautifulSoup(html, "html.parser")
 links = soup.find_all('a')
 
@@ -25,11 +29,11 @@ data = {}
 for link in links:
     data[link.text] = link.get("href")
 
+
 for key in data:
-    curs.execute("INSERT INTO crawler.urls (description, url) VALUES (%s, %s);", (key, data[key])) 
+    cursor.execute("INSERT INTO crawler.google (description, url) VALUES (%s, %s);", ( key.encode("utf-8"), data[key].encode("utf-8"))) 
 
-curs.close()
+# Auto commit is turned off by default
+connection.commit()
+cursor.close()
 connection.close()
-
-# TESTING
-print("done")
