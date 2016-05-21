b442877e77e890ec3c2c26515cc2ea4e2ebf0e6d
Timothy Pulliam <timothy.dpulliam@gmail.com>
2016-05-21 22:48:48 +0000

    Refactored crawler.py

--- crawler.py
+++ crawler.py
@@ -2,12 +2,13 @@
 
 import requests
 import MySQLdb
+import _mysql_exceptions
 from bs4 import BeautifulSoup
 
 # Setup the database connection.
 #url = input("Enter a URL for scraping links\n")
-url = "http://www.reddit.com"
-table_name = input("Enter the table name\n")
+url = input("Enter a URL for scraping links\n")
+table_name = input("Enter the MySQL table name to hold the scraped links\n")
 create_database_sql = "CREATE DATABASE IF NOT EXISTS crawler CHARACTER SET utf8;"
 create_table_sql = "CREATE TABLE IF NOT EXISTS crawler.%s (\
                     url_id int NOT NULL AUTO_INCREMENT,\
@@ -15,9 +16,17 @@ create_table_sql = "CREATE TABLE IF NOT EXISTS crawler.%s (\
                     url varchar(200),\
                     PRIMARY KEY (url_id));" %(table_name)
 
-connection = MySQLdb.connect(user="root", passwd="root", host="localhost", db="crawler")
-cursor = connection.cursor()
-cursor.execute(create_database_sql)
+# Try connecting to the already created 'crawler' DB. If this fails we must first create the crawler DB.
+try:
+    connection = MySQLdb.connect(user="root", passwd="root", host="localhost", db='crawler')
+    cursor = connection.cursor()
+except _mysql_exceptions.OperationalError as e:
+    print(e)
+    connection = MySQLdb.connect(user="root", passwd="root", host="localhost")
+    cursor = connection.cursor()
+    cursor.execute(create_database_sql)
+    cursor.execute('USE crawler;')
+    
 cursor.execute(create_table_sql)
 
 # start pulling html
@@ -30,9 +39,8 @@ data = {}
 for link in links:
     data[link.text] = link.get("href")
 
-
 for key in data:
-    cursor.execute("INSERT INTO %s (description, url) VALUES (%s, %s);", (table_name.encode("utf-8"), key.encode("utf-8"), data[key].encode("utf-8"))) 
+    cursor.execute("INSERT INTO "+ table_name +" (description, url) VALUES (%s, %s);", (key.encode("utf-8"), data[key].encode("utf-8"))) 
 # Auto commit is turned off by default
 connection.commit()
 cursor.close()
